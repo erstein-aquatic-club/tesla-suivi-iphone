11daaa50f962d7a3b8a0a3785cfda6bc28175489
erstein-aquatic-club <f.wagner1999@gmail.com>
2026-07-05 17:51:55 +0200

    Bump cache after layout fix

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='tesla-suivi-v3';
+const CACHE='tesla-suivi-v4';
 const ASSETS=['./','./index.html','./styles.css','./data.js','./app.js','./manifest.json','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
